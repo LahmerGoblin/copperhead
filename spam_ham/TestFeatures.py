ff1bb99a8ed69ecd8beb675ebfaea187931d3022
Arne Brüsch <a.bruesch@tu-bs.de>
2016-11-07 16:31:35 +0100

    new push :-)

--- spam_ham/TestFeatures.py
+++ spam_ham/TestFeatures.py
@@ -13,7 +13,8 @@ from IPython import embed
 from sklearn.pipeline import Pipeline
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
+#from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction import DictVectorizer
 
 import os
 import pickle
@@ -26,84 +27,101 @@ reader = DataReader('trainingsdata.zip')
 stemmer = PorterStemmer() 
 fold_runs = []
 
-if not os.path.isfile('train_features_' + str(k) + '.pickle'):
+if not os.path.isfile('train_features_' + str(k) + '_preextracted.pickle'):
         
     # k-fold cross validation
     # k defaults to 5
-    # TODO: k_folds does not work
     k_folds = reader.k_fold(k=k,rand=True)   
+
+    extracted_k_folds = []
+    for fold in k_folds:
+        feature_matrix = []
+        groundtruth_matrix = []
+        for mail in fold:
+            print('Performing Feature Extraction '+ mail[0])
+            tokenized_mail = word_tokenize(mail[1])
+            # remove stopwords and stemming
+            stemmed_mail_nostops = [ stemmer.stem_word(word) for word in tokenized_mail if word not in stopwords.words('english')]
+            # remove characters of length one - such as parentheses
+            stemmed_mail_nostops = [ word for word in stemmed_mail_nostops if len(word)>1]
+            # build dict
+            bow = {}
+            for word in stemmed_mail_nostops:
+                try:
+                    bow[word] += 1
+                except KeyError:
+                    bow[word] = 1
+            # get rid of prefix 
+            spam_ham = mail[0][len('spam1-train/'):]
+            #acquire groundtruth
+            groundtruth_matrix.append(reader.groundtruth_dict[spam_ham])
+            #extract_features
+            #n_grams = Features.n_grams(stemmed_mail_nostops,n=2)
+            #X.append(list(n_grams))
+            feature_matrix.append((mail[0],bow))
+        # does contain all k folds. For k = 5 len(extracted_k_folds) -> 5
+        extracted_k_folds.append((feature_matrix,groundtruth_matrix))
     
-    for test_fold in k_folds:
+    for i,test_fold in enumerate(k_folds):
         # exclude current test_fold
-        folds = [ fold for fold in k_folds if fold is not test_fold ]
+
         #X : {array-like, sparse matrix}, shape = [n_samples, n_features]
         #y : array-like, shape = [n_samples]
-        y = []
-        X = []
-        for fold in folds:
-            for mail in fold:
-                # TODO: Why is mail 00001 missing here? -> k_folds not working as it should
-                print('Performing Feature Extraction '+ mail[0])
-                tokenized_mail = word_tokenize(mail[1])
-                # remove stopwords and stemming
-                stemmed_mail_nostops = [ stemmer.stem_word(word) for word in tokenized_mail if word not in stopwords.words('english')]
-                # remove characters of length one - such as parentheses
-                stemmed_mail_nostops = [ word for word in stemmed_mail_nostops if len(word)>1]
-                
-    
-                # get rid of prefix 
-                spam_ham = mail[0][len('spam1-train/'):]
-                #acquire groundtruth
-                y.append(reader.groundtruth_dict[spam_ham])
-                #extract_features
-                #n_grams = Features.n_grams(stemmed_mail_nostops,n=2)
-                #X.append(list(n_grams))
-                X.append(stemmed_mail_nostops)
-    
-                # Which classifier to use? Word Embedding might perform best?
+        
+        # exclude current test_fold
+        extracted_folds_train = [ ext_fold for ext_fold in extracted_k_folds if ext_fold is not extracted_k_folds[i]]
+        ext_test_fold = extracted_k_folds[i]
         #determining vocab
-        features = set(itertools.chain(*X))
-        fold_runs.append((test_fold,folds,X,y,features))
+        # isolate words from trainingsset only
+        words = []
+        for fold in extracted_folds_train:
+            for mail in fold[0]:
+                words.append(mail[1].keys())
+        features = set(itertools.chain(*words))
+
+        fold_runs.append((ext_test_fold,extracted_folds_train,features))
     # pickle away
     pickle.dump(fold_runs,
-                codecs.open('train_features_' + str(k) + '.pickle', 'wb'))
+                codecs.open('train_features_' + str(k) + '_preextracted.pickle', 'wb'))
 else:
-    fold_runs = pickle.load(codecs.open('train_features_' + str(k) + '.pickle', 'rb'))
+    fold_runs = pickle.load(codecs.open('train_features_' + str(k) + '_preextracted.pickle', 'rb'))
 
 res_sets = []
 for run in fold_runs:
-    test_fold = run[0]
-    folds = run[1]
-    X = run[2]
-    y = run[3]
-    features = run[4] 
+    ext_test_fold = run[0]
+    ext_folds_train = run[1]
+    features = run[2] 
     
+    tokenized_mails = []
+    groundtruth = []
+    for fold in ext_folds_train:
+        for mail in fold[0]:
+            tokenized_mails.append(mail[1])
+        for gt in fold[1]:
+            groundtruth.append(gt)
+        
     # TODO: transform X into feature matrix
     print('No. of features ' + str(len(features)))
-    vectorizer = CountVectorizer(vocabulary=features)
+    vectorizer = DictVectorizer(vocabulary=features)
     # Now transform every tokenized mail back into one string
-    concat_X = []
-    for x in X:
-        concat_X.append(' '.join(x))
     # transform all the strings into a sparse matrix
-    sparse_X = vectorizer.fit_transform(concat_X)         
+    sparse_X = vectorizer.fit_transform(tokenized_mails)         
 
-    bayes = ('mbayes',MultinomialNB())
+    # train model 
+    #bayes = ('mbayes',MultinomialNB())
+    svm = ('svc',SVC(kernel='linear'))
     class_pipeline = Pipeline([bayes])
-    class_pipeline.fit(sparse_X,y)
+    class_pipeline.fit(sparse_X,groundtruth)
 
     res_set = {}
-    for mail in test_fold:
-        tokenized_mail = word_tokenize(mail[1])
-        # remove stopwords and stemming
-        stemmed_mail_nostops = [ stemmer.stem_word(word) for word in tokenized_mail if word not in stopwords.words('english')]
-        # remove characters of length one - such as parentheses
-        stemmed_mail_nostops = [ word for word in stemmed_mail_nostops if len(word)>1]
-        conc_mail = vectorizer.transform(' '.join(stemmed_mail_nostops))
-        
-        res_set[mail[0]] = class_pipeline.predict(conc_mail)
+    # evaluate each mail and add classification to res_set
+    for mail in ext_test_fold[0]:
+        joined_mail = ' '.join(mail[1])
+        mail_vec = vectorizer.fit_transform(joined_mail)
+        res_set[mail[0]] = class_pipeline.predict(mail_vec)
+
     res_sets.append(res_set)
-pickle.dump(res_sets,codecs.open('eval_' + str(k) + '_bayes.pickle'))
+pickle.dump(res_sets,codecs.open('eval_' + str(k) + '_bayes_preextracted.pickle','wb'))
 embed()
 
 
